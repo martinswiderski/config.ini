d85acd1e5c526b64e5f62bc14388288da8ab78be
Martin Swiderski <martinswiderski@googlemail.com>
2018-08-27 14:09:32 +0100

    triggering build error by failed tests

--- tests/specs/i8n-spec.js
+++ tests/specs/i8n-spec.js
@@ -7,7 +7,7 @@ describe('Reads object from a UTF 8 file', function () {
     it('UTF8 .ini file', function () {
         expect(configIni.encoding()).toBe('utf-8');
         expect(typeof i8nConfig).toBe('object');
-        expect(i8nConfig.Japan.miyamoto_musashi).toBe('宮本武蔵');
+        expect(i8nConfig.Japan.miyamoto_musashi).toBe('宮本武蔵 ');
         expect(i8nConfig.ME.salah_ad_din).toBe('صلاحالدينيوسفبنأيوب');
         expect(i8nConfig.Germany.gebhard_von_bluecher).toBe('Gebhard-Leberecht von Blücher Fürst von Wahlstatt');
     });
